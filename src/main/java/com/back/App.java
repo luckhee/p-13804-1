1913ea49bffe426ba46ab8e1d3ba7f0378c165b8
luckhee <dcs1206@naver.com>
2025-06-02 13:59:53 +0900

    7 g

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,7 +14,7 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
         int lastid=0;
-
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -25,9 +27,21 @@ public class App {
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
 
-                    WiseSaying wiseSaying = new WiseSaying(++lastid, author,  content);
+                    int id = ++lastid;
 
-                    System.out.println(++lastid +"번 명언이 등록되었습니다.");
+
+
+                    wiseSayings.add(new WiseSaying(id, author,content));
+
+                    System.out.printf("%d번 명언이 등록되었습니다.",id);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for(WiseSaying wiseSaying : wiseSayings.reversed()) {
+                        System.out.printf("%d / %s / %s\n",wiseSaying.getId(),wiseSaying.getAuthor(),wiseSaying.getContent());
+                    }
                 }
                 case "종료" -> {
                     return;
