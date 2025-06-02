32e72b95271c69df760264102f8f081aaa90c9fe
luckhee <dcs1206@naver.com>
2025-06-02 10:35:10 +0900

    work

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,21 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        lab1();
+    }
+
+    private static void lab1() {
+        String input = "등록\n나의 죽음을 적들에게 알리지 말라.";
+
+        Scanner scanner = new Scanner(input);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+
+        System.out.println("cmd : " + cmd);
+        System.out.println("content : " + content);
+    }
+}
