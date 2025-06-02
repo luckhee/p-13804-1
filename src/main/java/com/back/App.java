6be381444e5083bdf0a2ff806f4dcbdc202ad808
luckhee <dcs1206@naver.com>
2025-06-02 13:28:01 +0900

    5 g

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,36 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner scanner;
+
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String content = scanner.nextLine();
+
+                    System.out.print("작가 : ");
+                    String author = scanner.nextLine();
+
+                    System.out.println("1번 명언이 등록되었습니다.");
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+        }
+
+    }
+}
---
+++ src/test/java/com/back/AppTest.java
@@ -0,0 +1,47 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    @DisplayName("`== 명언 앱 ==` 출력")
+    void t1() {
+        String rs = AppTestRunner.run("");
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==");
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("명령) ")
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
+
+    @Test
+    @DisplayName("등록시 생성된 명언번호 노출")
+    void t3() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.");
+
+    }
+
+}
